61db00f49a9f615d1278139eec6f2de6d1fe8bd5
Ana Clara <ana146claraventurini@gmail.com>
2022-11-22 16:27:02 -0300

    Configurando variáveis de ambiente

--- livraria/settings.py
+++ livraria/settings.py
@@ -10,10 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
+
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 
 # Quick-start development settings - unsuitable for production
